61d453822b10bdb816dfbe79919e04c1500703bb
Tarthir <tylerbrady34@gmail.com>
2018-03-12 18:10:09 -0600

    Add files via upload

--- proxy.c
+++ proxy.c
@@ -16,6 +16,7 @@ typedef struct {
 	char resource[MAXLINE];
 	char protocol[MAXLINE];
 	char version[MAXLINE];
+	char host[MAXLINE];
 	int server_fd;
 }request_t;
 
@@ -23,6 +24,7 @@ typedef struct {
 static const char *user_agent_hdr = "User-Agent: Mozilla/5.0 (X11; Linux x86_64; rv:10.0.3) Gecko/20120305 Firefox/10.0.3\r\n";
 int handle_server_request(int connfd);
 int parse_request(request_t *request,char *buff);
+int send_request(int connfd,char *buff, rio_t rio, request_t  *req);
 
 
 
@@ -79,17 +81,11 @@ int main(int argc, char **argv)
 
 int handle_server_request(int connfd){
 	//Parts of the Request/etc
-	/*char method[MAXLINE];
-	char port[MAXLINE];
-	char resource[MAXLINE];
-	char protocol[MAXLINE];
-	char version[MAXLINE];*/
 	request_t request;
-/*	int server_fd;*/
-
 	//Holds the request
 	char buff[MAXLINE];
 	rio_t rio;
+
 	Rio_readinitb(&rio,connfd);
 	if(Rio_readlineb(&rio,buff,MAXLINE) < 0){
 		fprintf(stderr,"An error accord in handle_request");
@@ -102,8 +98,11 @@ int handle_server_request(int connfd){
 			fprintf(stderr,"An error accord in parse_request");
 			return -1;
 		}
+		else if(strstr(request.method,"GET") != NULL){
+			return send_request(connfd,buff,rio,&request);
+		}
 		else{
-			//return forward_request(buff,rio,request.server_fd,request.port);
+			//close the connection, this is not a GET
 		}
 	}
 	return 0;
@@ -117,6 +116,61 @@ int handle_server_request(int connfd){
 	if(read == 0){
 		read = -100;
 	}*/
+}
+
+int send_request(int connfd,char *buff, rio_t rio, request_t  *req){
+
+	int ret;
+	char sendbuf[MAXLINE];
+	while((ret = Rio_readlineb(&rio,buff,MAXLINE)) != 0){
+		//fprintf(stdout,"%s\n\n",buff);
+
+	//TODO send GET /hub/index.html HTTP/1.0 first
+		if(ret < 0){
+			fprintf(stderr,"Error reading request in send_request");
+		}
+		else if(strcmp(buff,"\r\n") == 0){
+			strcat(sendbuf,buff);
+			break;
+		}
+		if(strstr(buff,"Host:") != NULL){
+			//put port if needed
+			/*if(strstr(buff,":%d") == NULL){
+				buff--;
+				strcat(buff,":");
+				strcat(buff,req->port);
+			}*/
+			strcat(sendbuf,buff);
+		}
+		else if(strstr(buff,"Proxy-Connection:")){
+			strcat(sendbuf,"Proxy-Connection: close\r\n");
+		}
+		else if(strstr(buff,"Connection:")!= NULL){
+			strcat(sendbuf,"Connection: close\r\n");
+		}
+		else if(strstr(buff,"User-Agent") != NULL){
+			strcat(sendbuf,user_agent_hdr);
+		}
+		else{//anything else
+			strcat(sendbuf,buff);
+		}
+	}
+
+	//send of the request
+	if((req->server_fd = Open_clientfd(req->host,req->port)) == -1){
+		//no connection
+		return -1;
+	}
+	else if(req->server_fd == -2){
+		//tell client you gave me bad stuff
+		strcpy(buff,"BAD REQUEST");
+		Rio_writen(connfd,buff,strlen(buff));
+	}
+	else if(Rio_writen(req->server_fd,sendbuf,strlen(sendbuf)) < 0){
+		//write failed
+		return -1;
+	}
+	return 0;
 }
 
 int parse_request(request_t *request,char *buff){
@@ -137,6 +191,7 @@ int parse_request(request_t *request,char *buff){
 	}
 	char *ptr;
 	char * p = request->port;
+	//seperate out the port
 	if((ptr = strstr(request->port,":")) != NULL){
 		ptr++;
 		strncpy(p,ptr,sizeof(p));
@@ -145,5 +200,10 @@ int parse_request(request_t *request,char *buff){
 		memset(p, 0, MAXLINE);
 		strncpy(p,"80",2);
 	}
+	request->server_fd = -1;
+	//get the host name
+	char *t = strstr(request->url,"://");
+	t+=3;
+	sscanf(t,"%[^/|^:]",request->host);
 	return 0;
 }
