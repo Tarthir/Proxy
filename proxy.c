10e1c02f196550b91aabdd346ca3a6d653498e02
Tarthir <tylerbrady34@gmail.com>
2018-03-19 17:41:47 -0600

    Add files via upload
    Commented and cleaned up code

--- proxy.c
+++ proxy.c
@@ -6,7 +6,7 @@
 #define MAX_CACHE_SIZE 1049000
 #define MAX_OBJECT_SIZE 102400
 
-#define NUM_OF_THREADS 2
+#define NUM_OF_THREADS 3
 #define SBUFFSIZE 3
 #define	MAXLINE	 8192  /* Max text line length */
 #define MAXBUF   8192  /* Max I/O buffer size */
@@ -29,12 +29,13 @@ sbuf_t sbuf;//The buffer for our connections
 /* You won't lose style points for including this long line in your code */
 static const char *user_agent_hdr = "User-Agent: Mozilla/5.0 (X11; Linux x86_64; rv:10.0.3) Gecko/20120305 Firefox/10.0.3\r\n";
 int handle_server_request(int connfd, int *server_fd);
-int get_request(char *buff,char *port,char *resource,char *method,char *protocol,char *version,char *host);
-int send_request(int connfd,char *buff, rio_t rio, char *port,char *resource,char *method,char *protocol,char *version,char *host,int *server_fd);
+int get_request(char *buff,char *port,char *resource,char *method,char *host);
+int send_request(int connfd,char *buff, rio_t rio, char *port,char *resource,char *host,int *server_fd);
 void Cclose(int serverfd, int clientfd);
 int handle_response(int *server_fd,int clientfd);
-int read_response(rio_t *rio, int content_size,char *buf,int clientfd);
+int read_and_send_response(rio_t *rio, int content_size,char *buf,int clientfd);
 void *thread(void *vargp);
+void do_server_request(int connfd);
 
 
 
@@ -49,7 +50,8 @@ int main(int argc, char **argv)
 	Signal(SIGPIPE,SIG_IGN);
 	pthread_t tid;//tid for threads
 
-	/* Check command line args */
+	//check the command line arguments
+
 	if (argc != 2) {
 		fprintf(stderr, "usage: %s <port>\n", argv[0]);
 		exit(1);
@@ -64,7 +66,8 @@ int main(int argc, char **argv)
 	}
 	else{
 		listenfd = Open_listenfd(argv[1]);
-		sbuf_init(&sbuf,SBUFFSIZE);
+		sbuf_init(&sbuf,SBUFFSIZE); /**Was told it was okay to use this class by TA*/
+		//create the threads ahead of time to be put in pool
 		for(int i =0; i < NUM_OF_THREADS; i++){
 			Pthread_create(&tid,NULL,thread,NULL);
 		}
@@ -74,20 +77,8 @@ int main(int argc, char **argv)
 			while (1) {
 				clientlen = sizeof(clientaddr);
 				connfd = Accept(listenfd, (SA *)&clientaddr, &clientlen); //line:netp:tiny:accept
-				/*Getnameinfo((SA *) &clientaddr, clientlen, hostname, MAXLINE,
-							port, MAXLINE, 0);*/
-				//fprintf(stdout,"Connected to: %s on port %s",hostname,port);
-				//fflush(stdout);
-				//sleep(10);
-				//fprintf(stdout,"inserting fd:%d\n",connfd);
+				//we got a connection, insert file descriptor into fd pool
 				sbuf_insert(&sbuf,connfd);
-				//Seperate the hostname/query and all else.
-				//Then read response and forward it to the client
-				//something like GET "query that was sent to me" HTTP/1.0\r\n (so send them everything after the hostname, though make sure
-				// that 1.1's become 1.0s)
-				//Close(connfd);                                            //line:netp:tiny:close
-				//printf("%s", user_agent_hdr);
-				//Cclose(request.server_fd,connfd,hostname,port);
 			}
 		}
 		else{
@@ -99,30 +90,53 @@ int main(int argc, char **argv)
 	//Cclose(request.server_fd,connfd,hostname,port);
 	return 0;
 }
+
+/**This function handles the closing of the server and client file descriptors
+ * @param serverfd The server file descriptor
+ * @param clientfd The client file descriptor
+ * */
+void Cclose(int serverfd, int clientfd){
+	//fprintf(stdout,"Closed connections: (Server:%s,Client:%d)\n",serverfd,clientfd);
+	if(serverfd >= 0){
+		Close(serverfd);
+		serverfd = -1;
+	}
+	if(clientfd >= 0){
+		Close(clientfd);
+		clientfd = -1;
+	}
+
+}
+/**
+ * This function is called by each thread when it gets a connection
+ * No parameter is passed in since im using a connection pool
+ * */
 void *thread(void *vargp){
 	//Parts of the Request/etc
 	//sleep(10);
 	Pthread_detach(pthread_self());
 	//sleep(10);
-	/*//gets the name info
-	Getnameinfo((SA *) &clientaddr, clientlen, hostname, MAXLINE,
-			port, MAXLINE, 0);*/
 	//printf("Accepted connection from (%s, %s)\n", hostname, port);
 	while(1){
 		//request_t request;
-
+		//wait until a connection is available
 		int connfd = sbuf_remove(&sbuf);
 		if(connfd > 0){
+			//now that we have a connection, lets get some requests
 			do_server_request(connfd);
 		}
 	}
 	Pthread_exit(NULL);
 	return NULL;
 }
-
+/**This function controls the logic which handles the reading of the request we get from the client
+ * as well as sending that request of by calling two helper methods
+ * @param connfd The client connection file descriptor
+ * */
 void do_server_request(int connfd){
 	int server_fd = -1;
 	int val;
+	//handles parsing/reading/sending of client request to server
 	if((val = handle_server_request(connfd,&server_fd)) < 0){
 		if(val == -4){
 			fprintf(stderr,"Only Get request supported\n");
@@ -135,6 +149,7 @@ void do_server_request(int connfd){
 		//return val;
 	}
 	//if everything worked
+	//handles the parsing/reading/sending of server response to client
 	else if(val == 0 && handle_response(&server_fd,connfd) < 0){
 		if(val == -3){
 			fprintf(stderr,"An error while writing to client\n");
@@ -149,82 +164,16 @@ void do_server_request(int connfd){
 		Cclose(server_fd,connfd);
 	}*/
 }
-int handle_response(int *serverfd,int clientfd){
-	rio_t rio;
-	char buf[MAXLINE];//THIS VARIABLE is not safe, it holds the last request for some reason
-	int content_size;
-	//init the struct
-	Rio_readinitb(&rio,(*serverfd));
-	//grab first line
-	if(Rio_readlineb(&rio,buf,MAXLINE) < 0){
-		return -1;
-	}
-	//send off first line
-	else if(Rio_writen(clientfd,buf,strlen(buf)) < 0){
-		return -3;
-	}
-	else{
 
-		while(strcmp(buf,"\r\n") != 0 && strlen(buf) > 0){
-
-			if(Rio_readlineb(&rio,buf,MAXLINE) < 0){
-				return -1;
-			}
-			else{
-				//grab the content size
-				if(strstr(buf,"Content-Length")){
-					sscanf(buf,"Content-Length: %d",&content_size);
-				}
-				else if(strstr(buf,"Content-length")){
-					sscanf(buf,"Content-length: %d",&content_size);
-				}
-			}
-			if(Rio_writen(clientfd,buf,strlen(buf)) < 0){
-				return -3;
-			}
-		}
-	}
-	return read_response(&rio,content_size,buf,clientfd);
-}
-
-int read_response(rio_t *rio, int content_size,char *buf,int clientfd){
-	int num;
-	if(content_size > 0){
-		//if the size of the response is too big
-		while(content_size > MAXLINE){
-			if((num = Rio_readnb(rio,buf,MAXLINE)) < 0){
-				return -1;
-			}
-			else if(Rio_writen(clientfd,buf,num) == -1){
-				return -1;
-			}
-			else{
-				content_size = content_size - MAXLINE;
-			}
-		}
-		if(content_size > 0){
-			if(Rio_readnb(rio,buf,content_size) < 0){
-				return -1;
-			}
-			else if(Rio_writen(clientfd,buf,content_size) < 0){
-				return -1;
-			}
-		}
-	}
-	//no response
-	else{
-		while((num = Rio_readlineb(rio,buf,MAXLINE))){
-			if(Rio_writen(clientfd,buf,num) < 0){
-				return -1;
-			}
-		}
-	}
-	return 0;
-}
-
+/**
+ * Parses through and sends off the client request to the server
+ * @oaram connfd The client file descriptor
+ * @param serverfd The server file descriptor
+ * @return whether we were succesful or not
+ * */
 int handle_server_request(int connfd, int *server_fd){
-	//Holds the request
-	char buff[MAXLINE];
+	//Holds the request items
+	char buff[MAXLINE]; //holds what the client sent us
 	char port[MAXLINE];
 	char resource[MAXLINE];
 	char method[MAXLINE];
@@ -232,52 +181,99 @@ int handle_server_request(int connfd, int *server_fd){
 	rio_t rio;
 
 	Rio_readinitb(&rio,connfd);
+	//grab the first line of the get request
+	//EX: GET http://www.cmu.edu/hub/index.html HTTP/1.1
 	if(Rio_readlineb(&rio,buff,MAXLINE) < 0){
 		fprintf(stderr,"An error accord in handle_request\n");
 		return -1;
 	}
 	else{
-
-		//EX: GET http://www.cmu.edu/hub/index.html HTTP/1.0
-		char protocol[MAXLINE];
-		char version[MAXLINE];
-		if(get_request(buff,port,resource,method,protocol,version,host) < 0){
+		//parse through the request
+		if(get_request(buff,port,resource,method,host) < 0){
 			fprintf(stderr,"An error accord in parse_request\n");
 			return -1;
 		}
+		//send off the request
 		else if(strstr(method,"GET") != NULL){
-			return send_request(connfd,buff,rio,port,resource,method,protocol,version,host,server_fd);
+			return send_request(connfd,buff,rio,port,resource,host,server_fd);
 		}
 		else{
 			fprintf(stderr,"Only GET requests supported\n");
 			return -4;
-			//close the connection, this is not a GET
+			//this is not a GET
 		}
 	}
 	return 0;
-	/*ssize_t numRead = Rio_readlineb(connfd, res, byte_size);*/
-	/*
-	ssize_t read = recv(connfd,res_ptr,byte_size,0);
-	if(read < 0){
-		printf("%s","Error getting a response\n");
-		exit(errno);
-	}
-	if(read == 0){
-		read = -100;
-	}*/
 }
 
-int send_request(int connfd,char *buff, rio_t rio, char *port,char *resource,char *method,char *protocol,char *version,char *host,int *server_fd){
+/**
+ * Handles the parsing of the first line of the request
+ * @param buff The first line of the request
+ * @param port Variable which is to hold the port number
+ * @param resource Variable which is to hold the particular thing we want from the host
+ * @param host Will hold the hostname
+ * @return whether we were successful
+ * */
+int get_request(char *buff,char *port,char *resource,char *method,char *host){
+	char protocol[MAXLINE];
+	char version[MAXLINE];
+	//if invalid request
+	if(strlen(buff) < 1 || strstr(buff,"/") == NULL){
+		return -1;
+	}
+	char my_url[MAXLINE];
+	//EX GET http://www.cmu.edu/hub/index.html HTTP/1.1
+	strcpy(resource,"/");
+	//in order: GET,http://www.cmu.edu/hub/index.html,HTTP/1.1
+	sscanf(buff,"%s %s %s",method,my_url,version);
+	if(strstr(my_url,"://") != NULL){//if there is a url there
+		//so looking inside the url we are going to grab the below pieces of the URL
+		//in order http,www.cmu.edu,/hub/index.html
+		sscanf(my_url,"%[^:]://%[^/]%s",protocol,port,resource);
+	}
+	else{
+		sscanf(my_url,"%[^/]%s",port,resource);
+	}
+	char *ptr;
+	char * p = port;
+	//seperate out the port
+	if((ptr = strstr(port,":")) != NULL){
+		ptr++;
+		int size = sizeof(p);
+		strncpy(p,ptr,size);
+	}
+	else{
+		memset(p, 0, MAXLINE);
+		strncpy(p,"80",2);
+	}
+	//get the host name
+	char *t = strstr(my_url,"://");
+	if(t != NULL){
+		t+=3;
+		sscanf(t,"%[^/|^:]",host);
+	}
+	//reset buff
+	memset(buff,0,sizeof(char)*strlen(buff));
+	return 0;
+}
+/**Handles the sending of the request
+ * @param buff Holds the request
+ * @param rio The rio_t struct which helps read each line
+ * @param port Variable which is to hold the port numer
+ * @param resource Variable which is to hold the particular thing we want from the host
+ * @param host Will hold the hostname
+ * @param server_fd holds the server file descriptor
+ * @return whether we were successful or not
+ * */
+int send_request(int connfd,char *buff, rio_t rio, char *port,char *resource,char *host,int *server_fd){
 
 	int ret;
-	char sendbuf[MAXLINE];
+	char sendbuf[MAXLINE]; //holds what we are going to send to the server
 	strcat(sendbuf,"GET ");//TODO DOT ISSUE??
 	strcat(sendbuf,resource);
 	strcat(sendbuf," HTTP/1.0\r\n");
 	while((ret = Rio_readlineb(&rio,buff,MAXLINE)) != 0){
-		//fprintf(stdout,"%s\n\n",buff);
-
-		//TODO send GET /hub/index.html HTTP/1.0 first
+		//while there is more to read (until \r\n) replace things as needed and add everything to sendbuf
 		if(ret < 0){
 			fprintf(stderr,"Error reading request in send_request\n");
 		}
@@ -286,7 +282,6 @@ int send_request(int connfd,char *buff, rio_t rio, char *port,char *resource,cha
 			break;
 		}
 		if(strstr(buff,"Host:") != NULL){
-			//strcat(sendbuf,"Host: ");
 			strcat(sendbuf,buff);
 		}
 		else if(strstr(buff,"Proxy-Connection:")){
@@ -303,7 +298,7 @@ int send_request(int connfd,char *buff, rio_t rio, char *port,char *resource,cha
 		}
 	}
 
-	//send of the request
+	//send off the request
 	(*server_fd) = Open_clientfd(host,port);
 	if(*(server_fd) == -1){
 		//no connection
@@ -319,58 +314,100 @@ int send_request(int connfd,char *buff, rio_t rio, char *port,char *resource,cha
 		//write failed
 		return -1;
 	}
+	//reset
 	memset(sendbuf,0,strlen(sendbuf));
 	return 0;
 }
 
-int get_request(char *buff,char *port,char *resource,char *method,char *protocol,char *version,char *host){
-	if(strlen(buff) < 1 || strstr(buff,"/") == NULL){
+
+/**
+ * Handles the parsing/reading/sending off of the response from the server
+ * @param serverfd The server file descriptor
+ * @param clientfd The client file descriptor
+ * @return whether we were successful
+ * */
+int handle_response(int *serverfd,int clientfd){
+	rio_t rio;
+	char buf[MAXLINE];
+	int content_size;
+	//init the struct
+	Rio_readinitb(&rio,(*serverfd));
+	//grab first line
+	if(Rio_readlineb(&rio,buf,MAXLINE) < 0){
 		return -1;
 	}
-	char my_url[MAXLINE];
-	//EX GET http://www.cmu.edu/hub/index.html HTTP/1.0
-	strcpy(resource,"/");
-	//in ordeR: GET,http://www.cmu.edu/hub/index.html,HTTP/1.0
-	sscanf(buff,"%s %s %s",method,my_url,version);
-	if(strstr(my_url,"://") != NULL){//if there is a url there
-		//so looking inside the url we are going to grab the below pieces of the URL
-		//in order http,www.cmu.edu,/hub/index.html
-		sscanf(my_url,"%[^:]://%[^/]%s",protocol,port,resource);
+	//send off first line
+	else if(Rio_writen(clientfd,buf,strlen(buf)) < 0){
+		return -3;
 	}
 	else{
-		sscanf(my_url,"%[^/]%s",port,resource);
+		//while we don't hit the end of headers
+		while(strcmp(buf,"\r\n") != 0 && strlen(buf) > 0){
+
+			if(Rio_readlineb(&rio,buf,MAXLINE) < 0){
+				return -1;
+			}
+			else{
+				//grab the content size
+				if(strstr(buf,"Content-Length")){
+					sscanf(buf,"Content-Length: %d",&content_size);
+				}
+				//it sometimes has a lower case l
+				else if(strstr(buf,"Content-length")){
+					sscanf(buf,"Content-length: %d",&content_size);
+				}
+			}
+			//write it all to client
+			if(Rio_writen(clientfd,buf,strlen(buf)) < 0){
+				return -3;
+			}
+		}
 	}
-	char *ptr;
-	char * p = port;
-	//seperate out the port
-	if((ptr = strstr(port,":")) != NULL){
-		ptr++;
-		strncpy(p,ptr,sizeof(p));
-	}
-	else{
-		memset(p, 0, MAXLINE);
-		strncpy(p,"80",2);
-	}
-	//get the host name
-	char *t = strstr(my_url,"://");
-	if(t != NULL){
-		t+=3;
-		sscanf(t,"%[^/|^:]",host);
-	}
-	//reset buff
-	memset(buff,0,sizeof(char)*strlen(buff));
-	return 0;
+	//now handle the body
+	return read_and_send_response(&rio,content_size,buf,clientfd);
 }
 
-void Cclose(int serverfd, int clientfd){
-	//fprintf(stdout,"Closed connections: (Server:%s,Client:%d)\n",serverfd,clientfd);
-	if(serverfd >= 0){
-		Close(serverfd);
-		serverfd = NULL;
+/**
+ * Handles the reading and sending of the response to client
+ * @param rio The struct which helps us read line by line
+ * @param content_size Holds the amount we need to read
+ * @param buf The temp buffer we are putting the things we read in before it is sent
+ * @param clientfd The client file descriptor
+ * @return whether we were successful
+ * */
+int read_and_send_response(rio_t *rio, int content_size,char *buf,int clientfd){
+	int num;
+	if(content_size > 0){
+		//if the size of the response is too big, send it in chunks
+		while(content_size > MAXLINE){
+			if((num = Rio_readnb(rio,buf,MAXLINE)) < 0){
+				return -1;
+			}
+			else if(Rio_writen(clientfd,buf,num) == -1){
+				return -1;
+			}
+			else{
+				content_size = content_size - MAXLINE;
+			}
+		}
+		//now send the last bit of the response
+		if(content_size > 0){
+			if(Rio_readnb(rio,buf,content_size) < 0){
+				return -1;
+			}
+			else if(Rio_writen(clientfd,buf,content_size) < 0){
+				return -1;
+			}
+		}
 	}
-	if(clientfd >= 0){
-		Close(clientfd);
-		clientfd = NULL;
+	//no response
+	else{
+		//read anything that is there and send
+		while((num = Rio_readlineb(rio,buf,MAXLINE))){
+			if(Rio_writen(clientfd,buf,num) < 0){
+				return -1;
+			}
+		}
 	}
-
+	return 0;
 }
